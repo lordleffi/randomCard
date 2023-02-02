951cb6551c25025e6ca08bd3b9aac9678f30302d
LordLeffi <ledd_olsen@hotmail.com>
2023-02-02 13:33:03 -0800

    la til midtre og siste ord

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const begynnelse = ['jeg','du','han','hun','de'];
 const midten = ['er','løper','danser','spiser','kan','spiller','hopper'];
 const slutt = ['bra','rundt','på','av','ned','opp','alt','musikk'];
 
-console.log(`${begynnelse[Math.ceil(Math.random(begynnelse.lenght))]}`)
+console.log(`${begynnelse[Math.floor(Math.random() * begynnelse.length)]} ${midten[Math.floor(Math.random() * midten.length)]} ${slutt[Math.floor(Math.random() * slutt.length)]}`);
