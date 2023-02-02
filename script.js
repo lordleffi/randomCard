7020c81ea698f649797ff9b49dcf66c840b36930
LordLeffi <ledd_olsen@hotmail.com>
2023-02-02 13:26:24 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const begynnelse = ['jeg','du','han','hun','de'];
+const midten = ['er','løper','danser','spiser','kan','spiller','hopper'];
+const slutt = ['bra','rundt','på','av','ned','opp','alt','musikk'];
+
+console.log(`${begynnelse[Math.ceil(Math.random(begynnelse.lenght))]}`)
